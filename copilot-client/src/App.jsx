186b4f7f2be4fc67768caad2ac6dc7af4fda62b6
sovonbd <sovonsdp@gmail.com>
2024-03-08 15:56:17 -0600

    updated

--- copilot-client/src/App.jsx
+++ copilot-client/src/App.jsx
@@ -5,7 +5,6 @@ function App() {
     <>
       <h1 className="text-3xl text-blue-400 font-bold">The quick!</h1>
       <h1 className="text-3xl text-blue-400 font-bold font-neuzeit">The quick!</h1>
-      <h1 className="text-3xl text-blue-400 font-bold font-neuzeit">The quick!</h1>
     </>
   );
 }
