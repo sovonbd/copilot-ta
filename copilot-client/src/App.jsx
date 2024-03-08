12e51505896ac97cf09388f30ccff0d6446e56fa
sovonbd <sovonsdp@gmail.com>
2024-03-08 15:53:49 -0600

    added the custom fonts

--- copilot-client/src/App.jsx
+++ copilot-client/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <>
       <h1 className="text-3xl text-blue-400 font-bold">The quick!</h1>
       <h1 className="text-3xl text-blue-400 font-bold font-neuzeit">The quick!</h1>
+      <h1 className="text-3xl text-blue-400 font-bold font-neuzeit">The quick!</h1>
     </>
   );
 }
