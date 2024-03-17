1bf38a2709c80ee0fc0af70dbd3b2bfbe5a1a1d7
sovonbd <sovonsdp@gmail.com>
2024-03-17 00:54:05 -0500

    updated url

--- copilot-client/src/hooks/useAxiosPublic.jsx
+++ copilot-client/src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,8 @@
 // import { Axios } from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://18.222.146.145:5000",
+  baseURL: "http://ec2-18-222-146-145.us-east-2.compute.amazonaws.com",
+  withCredentials: true,
 });
 
 const useAxiosPublic = () => {
