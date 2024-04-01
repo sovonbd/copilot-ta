a4f4550501aaad6d7534ac2fc73ca1ad806c1d7a
MR. Sovon <sovonsdp@gmail.com>
2024-04-01 13:04:06 -0500

    updated url

--- copilot-client/src/hooks/useAxiosPublic.jsx
+++ copilot-client/src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 // import { Axios } from "axios";
 
 const axiosPublic = axios.create({
-  baseURL: "http://ec2-18-223-29-55.us-east-2.compute.amazonaws.com:5000",
+  baseURL: "http://ec2-3-145-65-7.us-east-2.compute.amazonaws.com:5000",
   // baseURL: "https://copilot-server.vercel.app",
   withCredentials: true,
 });
